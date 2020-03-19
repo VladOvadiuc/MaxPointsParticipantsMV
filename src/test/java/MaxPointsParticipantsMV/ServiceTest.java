d3401fb563a4ad68fb3d6ad856eab2dceb053ef9
Vlad Ovadiuc <vlad.ovadiuc@endava.com>
2020-03-19 22:09:32 +0200

    write functional tests

--- src/test/java/MaxPointsParticipantsMV/ServiceTest.java
+++ src/test/java/MaxPointsParticipantsMV/ServiceTest.java
@@ -1,10 +1,13 @@
 package MaxPointsParticipantsMV;
 
+import java.util.stream.StreamSupport;
+
 import org.junit.Test;
 
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
+import javafx.beans.value.ObservableBooleanValue;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
@@ -12,8 +15,11 @@ import service.Service;
 import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
+import validation.ValidationException;
 import validation.Validator;
 
+import static javafx.beans.binding.Bindings.when;
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 /**
@@ -21,60 +27,40 @@ import static junit.framework.TestCase.assertTrue;
  */
 public class ServiceTest {
 
+
+	Validator<Student> studentValidator = new StudentValidator();
+	Validator<Tema> temaValidator = new TemaValidator();
+	Validator<Nota> notaValidator = new NotaValidator();
+	StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "src/main/studentiTest.xml");
+	TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "src/main/temeTest.xml");
+	NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "src/main/noteTest.xml");
+	Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+
+	private long getSize(Iterable<Student> students) {
+		return StreamSupport.stream(students.spliterator(), false).count();
+	}
+
 	@Test
 	public void saveStudent() {
-		try {
-
-
-			Validator<Student> studentValidator = new StudentValidator();
-			Validator<Tema> temaValidator = new TemaValidator();
-			Validator<Nota> notaValidator = new NotaValidator();
-			StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-			TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-			NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-			Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-			service.saveStudent("29", "Ala bala", 932);
-			assertTrue(true);
-		}catch (Exception e)
-		{
-			assertTrue(false);
-		}
-
+		assertEquals(0, getSize(service.findAllStudents()));
+		assertEquals(1, service.saveStudent("1", "Name", 123));
+		assertEquals(1, getSize(service.findAllStudents()));
+		service.deleteStudent("1");
+		assertEquals(0, getSize(service.findAllStudents()));
 	}
 
 	@Test
 	public void saveStudentWithNullName(){
-		try {
-
-			Validator<Student> studentValidator = new StudentValidator();
-			Validator<Tema> temaValidator = new TemaValidator();
-			Validator<Nota> notaValidator = new NotaValidator();
-			StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-			TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-			NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-			Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-			service.saveStudent("29", null, 932);
-			assertTrue(false);
-		}catch (Exception e)
-		{
-			assertTrue(true);
-		}
+		assertEquals(0, getSize(service.findAllStudents()));
+		assertEquals(1, service.saveStudent("1", null, 123));
+		assertEquals(0, getSize(service.findAllStudents()));
 	}
 
 	@Test
-	public void saveStudentWithInvalidId() {
-		try {
-
-			Validator<Student> studentValidator = new StudentValidator();
-			Validator<Tema> temaValidator = new TemaValidator();
-			Validator<Nota> notaValidator = new NotaValidator();
-			StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-			TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-			NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
-			Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-			service.saveStudent("a", "Dunno", 932);
-			assertTrue(false);
-		} catch (Exception e) {
-			assertTrue(true);
-		}
-	}}
+	public void saveStudentWithInvalidGroup(){
+		assertEquals(0, getSize(service.findAllStudents()));
+		assertEquals(1, service.saveStudent("1", "Name", 1234));
+		assertEquals(0, getSize(service.findAllStudents()));
+	}
+}
